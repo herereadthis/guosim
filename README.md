93f77dd0a316001fab357a8ca93982d456b3a851
herereadthis <herereadthis.github@gmail.com>
2021-12-18 23:08:04 -0500

    guosim

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# guosim
+# Bump Action
 
-This github action will bump the semver of a nodejs repository, create the commit, and push the branch.
+This JavaScript based Github will update npm package files in in specified directories
 
 ## Inputs
 
-* `base_branch` the branch from which the new bumped branch will be created
-* `version` the semver of the new bump
+All inputs are required.
+
+* `base_branch` {string} the starting branch to make the version bump commit
+* `version` {string[major|minor|patch]} semver increment
+* `create_pull_request` {string[true|false]} whether to create a separate branch + pull request for commit
+* `package_directories` {string} whether to create a separate branch + pull request for commit
+
+## Outputs
+
+* `new_branch` {string} The name of the new branch. Unused if no pull request created
+* `new_version_number` {string} semver version of new bumped version
--- index.js
+++ index.js
@@ -1,57 +1,116 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const shell = require('shelljs');
-const semverRegex = require('semver-regex');
 const simpleGit = require('simple-git');
+const semver = require('semver');
+
+const package = require('./package.json');
 
 const git = simpleGit();
 
-shell.config.verbose = true;
+// Action inputs
+const VERSION = core.getInput('version');
+const BASE_BRANCH = core.getInput('base_branch');
+const CREATE_PULL_REQUEST = core.getInput('create_pull_request') === 'true';
+const PACKAGE_DIRECTORIES = JSON.parse(core.getInput('package_directories'));
 
-const versionNumber = core.getInput('version');
-const baseBranch = core.getInput('base_branch');
-const actor = github.context.actor;
-const email = `${actor}@email.com`;
+/**
+ * Default commit message. Be careful when modifying as this commit may be
+ * tracked by other workflows and/or bots.
+ * 
+ * @param {string} newVersionNumber 
+ * @returns {string} commit message
+ */
+const getCommitMessage = newVersionNumber => `Bump version to ${newVersionNumber}`;
 
-const newBranch = `bump-version-${versionNumber}`
+/**
+ * Gets new version number by semver.
+ * 
+ * @returns (string) semver version
+ */
+const getNewVersion = () => {
+    if (!['major', 'minor', 'patch'].includes(VERSION)) {
+        throw new Error('invalid version!');
+    }
 
-console.log(`Version number is: ${versionNumber}`);
-console.log(`Base Branch is: ${baseBranch}`);
-console.log(`New Branch is: ${newBranch}`);
-console.log(`Actor is ${actor}`);
-console.log(`Email is ${email}`);
+    return semver.inc(package.version, VERSION);
+};
 
-// console.log('github context');
-// console.log(github.context);
+/**
+ * Update all package files based on package directory list
+ */
+const updatePackages = () => {
+    PACKAGE_DIRECTORIES.forEach((directory) => {
+        shell.exec(`cd ${directory};npm version ${VERSION} --no-git-tag-version`);
+    });
+};
 
-if (!semverRegex().test(versionNumber)) {
-    throw new Error('Version number is not semver!');
+/**
+ * Set up configuration and local branch
+ * User is github actions-bot
+ * https://github.com/actions/checkout#push-a-commit-using-the-built-in-token
+ * 
+ * @param {string} baseBranch - where to start
+ * @returns {function}
+ */
+const initializeGit = (baseBranch) => {
+    return git
+        .addConfig('user.email', 'github-actions')
+        .addConfig('user.name', 'github-actions@github.com')
+        .fetch()
+        .checkout(baseBranch);
 }
 
-const configure = async () => {
+/**
+ * Bump package versions and push the commit.
+ * 
+ * @param {string} baseBranch - the starting branch at which to make the commit
+ * @param {string} commitMessage - text for commit
+ */
+const pushVersionBump = async (baseBranch, commitMessage) => {
     try {
+        await initializeGit(baseBranch);
+        updatePackages();
         await git
-            .addConfig('user.email', email)
-            .addConfig('user.name', actor)
-            .addConfig('author.email', email)
-            .addConfig('author.name', actor)
-            .addConfig('committer.email', email)
-            .addConfig('committer.name', actor)
-            .checkoutBranch(newBranch, baseBranch);
-
-        shell.exec(`npm version ${versionNumber} --no-git-tag-version`);
-
-        await git
-            .add('package.json')
-            .add('package-lock.json')
-            .commit(`Bump version ${versionNumber}`)
-            .push('origin', newBranch);
-
-        console.log(`Pushed branch ${newBranch}`);
+            .add('.')
+            .commit(commitMessage)
+            .push('origin', baseBranch);
     } catch (err) {
+        core.setFailed(err.message);
         throw err;
     }
 };
 
-configure();
+/**
+ * Bump package versions in a new branch and create a pull request.
+ * 
+ * @param {string} baseBranch - the starting branch at which to create a new branch
+ * @param {string} newBranch - the name of the new branch
+ * @param {string} commitMessage - text for commit
+ */
+const createVersionBumpPullRequest = async (baseBranch, newBranch, commitMessage) => {
+    try {
+        await initializeGit(baseBranch);
+        await git.checkoutLocalBranch(newBranch);
+        updatePackages();
+        await git
+            .add('.')
+            .commit(commitMessage)
+            .push('origin', newBranch);
+    } catch (err) {
+        core.setFailed(err.message);
+        throw err;
+    }
+}
 
+const newVersionNumber = getNewVersion();
+const newBranch = `bump-version-${newVersionNumber}`;
+const commitMessage = getCommitMessage(newVersionNumber);
+
+core.setOutput("new_version_number", newVersionNumber);
+core.setOutput("new_branch", newBranch);
+
+if (CREATE_PULL_REQUEST) {
+    createVersionBumpPullRequest(BASE_BRANCH, newBranch, commitMessage);
+} else {
+    pushVersionBump(BASE_BRANCH, commitMessage);
+}
